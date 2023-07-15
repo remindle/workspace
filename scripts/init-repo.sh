5e22ac6e620f5580eb242a13bf38bd5984c446a5
Leo van Haaren <814676+leovanhaaren@users.noreply.github.com>
2023-07-15 16:29:41 +0200

    feat: add repo initialization script

---
+++ scripts/init-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create a new repository
+echo "# $2" >> README.md
+
+cat >> README.md << EOL
+
+## Description
+$3
+EOL
+
+git init
+git add README.md
+git commit -m "feat: first commit"
+git branch -M main
+git remote add origin git@github.com:$1/$2.git
+git push -u origin main -f
